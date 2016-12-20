657b1198cf0be4838299301ec7306b6de02c80f8
david <david.gonchar@gmail.com>
2016-12-20 17:26:39 +0200

    works with sdcc cluster replication set

--- sdcc_manage.sh
+++ sdcc_manage.sh
@@ -6,27 +6,19 @@ MONGODB_DIR=/data/mongodb
 MONGODB_LOG=/var/log/mongodb.log
 
 if [ $# != 2 ]; then 
-echo "Usage: $0 <remote host IP, e.g. 10.142.0.3> <local default role (master|slave), e.g. master>"
+echo "Usage: $0 <remote host 1, e.g. 10.142.0.3> <remote host 2, e.g. 10.142.0.4>"
 exit 1
 fi
 
 echo "=============== (`date`) Start script ==============="
 
-REMOTE_HOST=$1
-LOCAL_ROLE=$2
+REMOTE_HOST1=$1
+REMOTE_HOST2=$2
 
 # Start MongoDB server
 function start_mongo()
 {
 echo "Starting MongoDB server (`date`)"
-if [ "$LOCAL_ROLE" == "master" ]; then
-sudo mongod --fork --logpath $MONGODB_LOG --dbpath $MONGODB_DIR --master
-elif [ "$LOCAL_ROLE" == "slave" ]; then
-sudo mongod --fork --logpath $MONGODB_LOG --dbpath $MONGODB_DIR --slave --source $REMOTE_HOST
-else
-echo "Could not start MongoDB, incorrect role: $LOCAL_ROLE"
-return
-fi
 echo "MongoDB started (`date`)"
 }
 
@@ -34,8 +26,6 @@ echo "MongoDB started (`date`)"
 function stop_mongo()
 {
 echo "Stopping MongoDB server (`date`)"
-sudo mongod --shutdown --dbpath $MONGODB_DIR_DEFAULT
-sudo mongod --shutdown --dbpath $MONGODB_DIR
 echo "MongoDB stopped (`date`)"
 }
 
@@ -58,43 +48,27 @@ echo "SDCC services stopped (`date`)"
 #
 # Check MongoDB running and the running role on the both Local and Remote servers
 #
-mongo_res=`fab --hide=running,warning,aborts,stderr,status -H $LOCAL_HOST,$REMOTE_HOST -- $HOME/scripts/mon_mongodb.sh | awk -v home=$HOME '/Master/ {if($4 == "true"){print $1" master"} else if($4 == "false"){print $1" slave"} else {t=strftime("%Y%m%d_%H-%M-%S",systime());print $1" failure";print $1" failure" >> home"/scripts/logs/mon_mongodb_"t".err"}} /Fatal error/{print $0}'`
+mongo_res=`fab --hide=running,warning,aborts,stderr,status -H ${LOCAL_HOST},${REMOTE_HOST1},${REMOTE_HOST2} -- $HOME/scripts/mon_mongodb.sh | awk -v home=$HOME '/Master/ {if($4 == "true"){print $1" master"} else if($4 == "false"){print $1" slave"} else {t=strftime("%Y%m%d_%H-%M-%S",systime());print $1" failure";print $1" failure" >> home"/scripts/logs/mon_mongodb_"t".err"}} /Fatal error/{print $0}'`
 
 # Local role of MongoDB
-local_role=`echo "$mongo_res" | awk -v host=$LOCAL_HOST '{if($1 && index($1,host))print $2}'`
+local_role=`echo "$mongo_res" | awk -v host=${LOCAL_HOST} '{if($1 && index($1,host))print $2}'`
 echo "Local: $local_role"
 
 # Remote role of MongoDB
-remote_role=`echo "$mongo_res" | awk -v host=$REMOTE_HOST '{if($1 && index($1,host))print $2}'`
-echo "Remote: $remote_role"
+remote_role1=`echo "$mongo_res" | awk -v host=${REMOTE_HOST1} '{if($1 && index($1,host))print $2}'`
+echo "Remote 1: $remote_role1"
 
-#
-# Manage incorrect role cases 
-#
-#if [ "$local_role" == "$remote_role" ]; then
-#
-## Send alert, the same role on the both MongoDB servers 
-#
-#  if [ "$local_role" == "$LOCAL_ROLE" ]; then
-#    echo "Local role is correct, do nothing: active ($local_role), defined ($LOCAL_ROLE)"
-#  else
-#    echo "Local role is incorrect, restart MongoDB: active ($local_role), defined ($LOCAL_ROLE)"
-#    stop_mongo
-#    start_mongo
-#  fi
-#elif [ "$local_role" == "failure" ]; then
-#echo "Local MongoDB is not running, start it"
-#start_mongo
-#fi
+remote_role2=`echo "$mongo_res" | awk -v host=${REMOTE_HOST2} '{if($1 && index($1,host))print $2}'`
+echo "Remote 2: $remote_role2"
 
 #
 # Manage SDCC service
 #
 
-if [ "$local_role" != "master" ] && [ "$remote_role" == "master" ]; then
+if [ "$local_role" != "master" ]; then
 echo "Local: $local_role, stopping SDCC services"
 stop_sdcc
-elif [ "$local_role" == "master" ] && [ "$remote_role" != "master" ]; then
+elif [ "$local_role" == "master" ]; then
 echo "Local: $local_role, starting SDCC services"
 start_sdcc
 fi
